b4f366b8f1e83b0e6073e977c850d2202622004c
i-ape <i-ape@proton.me>
2024-10-05 04:37:12 +0200

    diesel migration run

--- src/schema.rs
+++ src/schema.rs
@@ -24,26 +24,3 @@ diesel::allow_tables_to_appear_in_same_query!(
     fasting_sessions,
     users,
 );
-
-
-table! {
-    users (id) {
-        id -> Integer,
-        username -> Text,
-        password -> Text,
-    }
-}
-
-table! {
-    fasting_sessions (id) {
-        id -> Integer,
-        user_id -> Integer,
-        started_at -> Timestamp,
-        stopped_at -> Nullable<Timestamp>,
-    }
-}
-
-allow_tables_to_appear_in_same_query!(
-    users,
-    fasting_sessions,
-);
