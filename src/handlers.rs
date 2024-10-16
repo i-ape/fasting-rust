1364d619d047c7073d23b38d38b6d42176e3da46
i-ape <i-ape@proton.me>
2024-10-16 03:23:01 +0200

    handlers updated

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,11 +1,11 @@
 use bcrypt::{hash, verify, DEFAULT_COST};
+use chrono::{NaiveDateTime, Utc};
 use diesel::prelude::*;
 use diesel::SqliteConnection;
 
 use crate::models::{FastingEvent, NewFastingEvent, NewUser, User};
 use crate::schema::fasting_events::dsl::*;
 use crate::schema::users::dsl::*;
-use chrono::{NaiveDateTime, Utc};
 
 /// Create a new user in the database, with hashed password
 pub fn create_user(
@@ -14,11 +14,11 @@ pub fn create_user(
     password_input: &str,
 ) -> Result<usize, diesel::result::Error> {
     // Hash the password before storing it
-    let hashed_password = hash(password, DEFAULT_COST).expect("Error hashing password");
+    let hashed_password = hash(password_input, DEFAULT_COST).expect("Error hashing password");
 
     let new_user = NewUser {
-        username: str,
-        hashed_password: str,
+        username: username_input.to_string(),
+        hashed_password,
     };
 
     diesel::insert_into(users).values(&new_user).execute(&conn)
@@ -49,12 +49,11 @@ pub fn start_fasting(
     user_id_input: i32,
     start_time: NaiveDateTime,
     stop_time: NaiveDateTime,
-
 ) -> Result<usize, diesel::result::Error> {
     let new_event = NewFastingEvent {
         user_id: user_id_input,
         start_time,
-        stop_time, // fast is ongoing
+        stop_time: Some(stop_time), // Can be None if the fast is ongoing
     };
 
     diesel::insert_into(fasting_events)
@@ -68,8 +67,8 @@ pub fn stop_fasting(
     event_id: i32,
     stop_time: NaiveDateTime,
 ) -> Result<usize, diesel::result::Error> {
-    // Update the fasting event, setting the end_time to stop_time
+    // Update the fasting event, setting the stop_time to the provided stop_time
     diesel::update(fasting_events.find(event_id))
-        .set(end_time.eq(Some(stop_time)))
+        .set(stop_time.eq(Some(stop_time)))
         .execute(conn)
 }
