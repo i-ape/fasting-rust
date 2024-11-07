691b5aef34345f973677f66eeaa921be351cb050
i-ape <i-ape@proton.me>
2024-11-07 03:13:29 +0100

    created lib.rs, fixed my test folder issue

--- tests/handlers_tests.rs
+++ tests/handlers_tests.rs
@@ -1,9 +1,10 @@
-use chrono::NaiveDateTime;
+use fasting_rust::handlers::{create_user, find_user_by_username, login_user, start_fasting, stop_fasting};
+use fasting_rust::models::{NewUser, User};
+use fasting_rust::errors::FastingAppError;
+use fasting_rust::schema::users::dsl::*;
 use diesel::connection::Connection;
 use diesel::sqlite::SqliteConnection;
-use fasting_app::handlers::{create_user, login_user, start_fasting, stop_fasting};
-use fasting_app::models::NewUser;
-use fasting_app::schema::users::dsl::*;
+use chrono::NaiveDateTime;
 
 fn establish_test_connection() -> SqliteConnection {
     let conn = SqliteConnection::establish(":memory:").unwrap();
