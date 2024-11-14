25e9b12597532c328927db0859c4cbc7d05e3d0a
i-ape <i-ape@proton.me>
2024-11-14 02:46:51 +0100

    fasting & user logic added

--- src/handlers/user.rs
+++ src/handlers/user.rs
@@ -5,13 +5,36 @@ use bcrypt::{hash, verify, DEFAULT_COST};
 use diesel::prelude::*;
 use diesel::SqliteConnection;
 
+pub fn create_user(
+    conn: &mut SqliteConnection,
+    username_input: &str,
+    password_input: &str,
+) -> Result<usize, FastingAppError> {
+    let hashedp = hash(password_input, DEFAULT_COST).map_err(FastingAppError::PasswordHashError)?;
+    // Create a new user struct with a different variable name
+    let new_user = NewUser {
+        username: username_input.to_string(),
+        hashed_password: hashedp,
+    };
+    diesel::insert_into(users)
+        .values(&new_user)
+        .execute(conn)
+        .map_err(FastingAppError::DatabaseError)
+}
 /// Registers a new user
 pub fn register_user(
     conn: &mut SqliteConnection,
     username_input: &str,
     password_input: &str,
 ) -> Result<String, FastingAppError> {
-    // Registration logic
+    create_user(conn, username_input, password_input)
+        .map(|_| "User created successfully".to_string())
+        .map_err(|e| match e {
+            FastingAppError::DatabaseError(diesel_error) => {
+                FastingAppError::DatabaseError(diesel_error)
+            }
+            _ => e,
+        })
 }
 
 /// Logs in a user by verifying credentials
@@ -20,7 +43,13 @@ pub fn login_user(
     username_input: &str,
     password_input: &str,
 ) -> Result<User, FastingAppError> {
-    // Login logic
+    
+    let user = find_user_by_username(conn, username_input)?;
+    if verify(password_input, &user.hashed_password).map_err(FastingAppError::PasswordHashError)? {
+        Ok(user)
+    } else {
+        Err(FastingAppError::InvalidCredentials)
+    }
 }
 
 /// Updates user profile details
@@ -30,5 +59,22 @@ pub fn update_user_profile(
     new_username: Option<&str>,
     new_password: Option<&str>,
 ) -> Result<usize, FastingAppError> {
-    // Profile update logic
+    
+    let mut query = diesel::update(users).into_boxed();
+
+    if let Some(username) = new_username {
+        query = query.set(username.eq(username.to_string()));
+    }
+
+    if let Some(password) = new_password {
+        
+    let hashedp = hash(password_input, DEFAULT_COST).map_err(FastingAppError::PasswordHashError)?;
+    // Create a new user struct with a different variable name
+    let new_user = NewUser {
+        username: username_input.to_string(),
+        hashed_password: hashedp,
+    };
+    }
+
+    query.execute(conn).map_err(FastingAppError::DatabaseError)
 }
