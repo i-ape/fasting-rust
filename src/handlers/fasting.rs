1ff0f85896ec57e4613d6531abc35a85bcccfc99
i-ape <i-ape@proton.me>
2024-11-14 02:24:48 +0100

    fasting logic added

--- src/handlers/fasting.rs
+++ src/handlers/fasting.rs
@@ -11,8 +11,20 @@ pub fn start_fasting(
     user_id: i32,
     event_start_time: NaiveDateTime,
 ) -> Result<usize, FastingAppError> {
-    // Start fasting logic
-}
+    if find_active_fasting_event(conn, user_id_input)?.is_some() {
+        return Err(FastingAppError::ExistingSessionError);
+    }
+
+    let new_event = NewFastingEvent {
+        user_id: user_id_input,
+        start_time: event_start_time,
+        stop_time: None,
+    };
+
+    diesel::insert_into(fasting_events)
+        .values(&new_event)
+        .execute(conn)
+        .map_err(FastingAppError::DatabaseError)
 
 /// Stops the active fasting event
 pub fn stop_fasting(
@@ -20,7 +32,15 @@ pub fn stop_fasting(
     user_id: i32,
     end_time: NaiveDateTime,
 ) -> Result<usize, FastingAppError> {
-    // Stop fasting logic
+    diesel::update(
+        fasting_events
+            .filter(user_id.eq(user_id_input))
+            .filter(stop_time.is_null()),
+    )
+    .set(stop_time.eq(Some(end_time_input)))
+    .execute(conn)
+    .map_err(FastingAppError::DatabaseError)
+    
 }
 
 /// Retrieves the current fasting status
@@ -28,5 +48,10 @@ pub fn get_current_fasting_status(
     conn: &mut SqliteConnection,
     user_id: i32,
 ) -> Result<Option<(NaiveDateTime, i64)>, FastingAppError> {
-    // Get fasting status logic
+    fasting_events
+        .filter(user_id.eq(user_id_input))
+        .filter(stop_time.is_null())
+        .first::<FastingEvent>(conn)
+        .optional()
+        .map_err(FastingAppError::DatabaseError)
 }
