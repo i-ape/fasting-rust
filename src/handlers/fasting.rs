0c4fa338a91c41eb36a4a349b6d26ca72a28c2ad
i-ape <i-ape@proton.me>
2024-11-14 02:38:24 +0100

    renamed some vars and fn

--- src/handlers/fasting.rs
+++ src/handlers/fasting.rs
@@ -11,12 +11,14 @@ pub fn start_fasting(
     user_id: i32,
     event_start_time: NaiveDateTime,
 ) -> Result<usize, FastingAppError> {
-    if find_active_fasting_event(conn, user_id_input)?.is_some() {
+    // Check if there's an active fasting event
+    if find_active_fasting_event(conn, user_id)?.is_some() {
         return Err(FastingAppError::ExistingSessionError);
     }
 
+    // Create and insert a new fasting event
     let new_event = NewFastingEvent {
-        user_id: user_id_input,
+        user_id,
         start_time: event_start_time,
         stop_time: None,
     };
@@ -25,6 +27,7 @@ pub fn start_fasting(
         .values(&new_event)
         .execute(conn)
         .map_err(FastingAppError::DatabaseError)
+}
 
 /// Stops the active fasting event
 pub fn stop_fasting(
@@ -34,13 +37,12 @@ pub fn stop_fasting(
 ) -> Result<usize, FastingAppError> {
     diesel::update(
         fasting_events
-            .filter(user_id.eq(user_id_input))
+            .filter(user_id.eq(user_id))
             .filter(stop_time.is_null()),
     )
-    .set(stop_time.eq(Some(end_time_input)))
+    .set(stop_time.eq(Some(end_time)))
     .execute(conn)
     .map_err(FastingAppError::DatabaseError)
-    
 }
 
 /// Retrieves the current fasting status
@@ -48,8 +50,25 @@ pub fn get_current_fasting_status(
     conn: &mut SqliteConnection,
     user_id: i32,
 ) -> Result<Option<(NaiveDateTime, i64)>, FastingAppError> {
+    // Retrieve the active fasting event if any
+    if let Some(event) = find_active_fasting_event(conn, user_id)? {
+        let start_time = event.start_time;
+        let duration = chrono::Utc::now().naive_utc()
+            .signed_duration_since(start_time)
+            .num_minutes();
+        Ok(Some((start_time, duration)))
+    } else {
+        Ok(None)
+    }
+}
+
+/// Helper function to find an active fasting event for a specific user
+fn find_active_fasting_event(
+    conn: &mut SqliteConnection,
+    user_id: i32,
+) -> Result<Option<FastingEvent>, FastingAppError> {
     fasting_events
-        .filter(user_id.eq(user_id_input))
+        .filter(user_id.eq(user_id))
         .filter(stop_time.is_null())
         .first::<FastingEvent>(conn)
         .optional()
