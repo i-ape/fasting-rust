1bf7ddfb77caae1b8d59a328d7fc8993adf2d98a
i-ape <i-ape@proton.me>
2024-11-21 02:19:15 +0100

    find .rs added

---
+++ src/users/find.rs
@@ -0,0 +1,18 @@
+use crate::errors::FastingAppError;
+use crate::models::User;
+use crate::schema::users::dsl::{username, users};
+use diesel::prelude::*;
+use diesel::SqliteConnection;
+
+/// Finds a user by their username in the database.
+pub fn find_user_by_username(
+    conn: &mut SqliteConnection,
+    username_input: &str,
+) -> Result<User, FastingAppError> {
+    users
+        .filter(username.eq(username_input))
+        .first::<User>(conn)
+        .optional()
+        .map_err(FastingAppError::DatabaseError)?
+        .ok_or(FastingAppError::InvalidCredentials)
+}
