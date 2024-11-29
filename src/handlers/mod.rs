16f4eb84d79a1444ac1b7b4e5447c03637be9d59
i-ape <i-ape@proton.me>
2024-11-29 02:34:48 +0100

    imports experiment

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -2,5 +2,5 @@ pub mod analytics;
 pub mod fasting;
 
 // Re-export functions for easier access
-pub use analytics::calculate_average_fasting_duration;
-pub use fasting::{start_fasting, stop_fasting};
+pub use analytics::*;
+pub use fasting::*;
