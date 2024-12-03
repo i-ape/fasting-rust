57b928a3433bc1b14d04e1f945002538e0ce39b0
i-ape <i-ape@proton.me>
2024-12-03 03:15:32 +0100

    login comments & 3 errors rm, tomorrow device_id needs to be fixed

--- src/users/login.rs
+++ src/users/login.rs
@@ -1,24 +1,27 @@
 use crate::errors::FastingAppError;
 use crate::models::User;
 use bcrypt::verify;
-use diesel::SqliteConnection;
+use diesel::prelude::*;
 use crate::schema::users::dsl::{device_id, id, users};
 use super::find::find_user_by_username;
 
-/// Logs in a user by verifying their credentials.
+/// Logs in a user by verifying their username and password.
 pub fn login_user(
     conn: &mut SqliteConnection,
     username_input: &str,
     password_input: &str,
 ) -> Result<User, FastingAppError> {
     let user = find_user_by_username(conn, username_input)?;
+
+    // Verify the password
     if verify(password_input, &user.hashed_password).map_err(FastingAppError::PasswordHashError)? {
         Ok(user)
     } else {
         Err(FastingAppError::InvalidCredentials)
     }
 }
-/// Finds a user by their device ID
+
+/// Finds a user by their device ID.
 pub fn find_user_by_device_id(
     conn: &mut SqliteConnection,
     device_id_input: &str,
@@ -30,30 +33,43 @@ pub fn find_user_by_device_id(
         .map_err(FastingAppError::DatabaseError)?
         .ok_or(FastingAppError::InvalidCredentials)
 }
-/// Associates a device ID with a user
+
+/// Associates a device ID with a user account.
 pub fn associate_device_id(
     conn: &mut SqliteConnection,
     user_id: i32,
     device_id_input: &str,
 ) -> Result<usize, FastingAppError> {
+    if device_id_input.is_empty() {
+        return Err(FastingAppError::InvalidRequest(
+            "Device ID cannot be empty.".to_string(),
+        ));
+    }
+
     diesel::update(users.filter(id.eq(user_id)))
         .set(device_id.eq(device_id_input))
         .execute(conn)
         .map_err(FastingAppError::DatabaseError)
 }
-/// Login using either username/password or device ID
+
+/// Logs in using either username/password or device ID.
 pub fn login_user_or_device(
     conn: &mut SqliteConnection,
     username_input: Option<&str>,
     password_input: Option<&str>,
     device_id_input: Option<&str>,
 ) -> Result<User, FastingAppError> {
-    if let Some(device_id) = device_id_input {
-        return find_user_by_device_id(conn, device_id);
-    }
-
-    if let (Some(username), Some(password)) = (username_input, password_input) {
-        return super::login::login_user(conn, username, password);
+    match device_id_input {
+        Some(device_id) => {
+            // Login using device ID
+            return find_user_by_device_id(conn, device_id);
+        }
+        None => {
+            if let (Some(username), Some(password)) = (username_input, password_input) {
+                // Login using username and password
+                return login_user(conn, username, password);
+            }
+        }
     }
 
     Err(FastingAppError::InvalidRequest(
