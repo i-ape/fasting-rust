80430a9ae895ee3156d979637e3244329ff2ea96
i-ape <i-ape@proton.me>
2024-12-23 01:01:44 +0100

    handlers menu, let _ = conn;, this feels like a temporary fix

--- src/handlers/menu.rs
+++ src/handlers/menu.rs
@@ -1,15 +1,18 @@
 use diesel::SqliteConnection;
-
 pub fn display_main_menu(conn: &mut SqliteConnection) {
+    let _ = conn;
+    
     // Code to display the main menu and handle actions.
     println!("Main Menu is displayed here.");
     // Call specific handlers like fasting, analytics, etc.
 }
 
 pub fn handle_fasting_menu(conn: &mut SqliteConnection) {
+    let _ = conn;
     println!("Fasting menu logic.");
 }
 
 pub fn handle_analytics_menu(conn: &mut SqliteConnection) {
+    let _ = conn;
     println!("Analytics menu logic.");
 }
