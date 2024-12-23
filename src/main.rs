4880b1417bd787dece4af835487bee48c3f00510
i-ape <i-ape@proton.me>
2024-12-23 01:26:07 +0100

    handlers menu, main changed

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,12 @@ extern crate bcrypt;
 extern crate diesel;
 extern crate dotenv;
 
-use std::io::{self, Write};
-use chrono::NaiveDateTime;
 use dotenv::dotenv;
+use log;
 
 use crate::db::establish_connection;
 use crate::errors::handle_error;
-use crate::handlers::menu::{
-    handle_register, handle_login, handle_update, handle_fasting_menu, handle_analytics_menu,
-};
-use log;
+use crate::handlers::menu::display_main_menu;
 
 mod db;
 mod errors;
@@ -21,9 +17,11 @@ mod schema;
 mod users;
 
 fn main() {
+    // Load environment variables and initialize logging
     dotenv().ok();
     env_logger::init();
 
+    // Establish a database connection
     let mut conn = match establish_connection() {
         Ok(connection) => connection,
         Err(e) => {
@@ -33,47 +31,6 @@ fn main() {
         }
     };
 
-    // Main menu loop
-    loop {
-        match select_menu_option() {
-            MenuOption::Register => handle_register(&mut conn),
-            MenuOption::Login => handle_login(&mut conn),
-            MenuOption::Update => handle_update(&mut conn),
-            MenuOption::Fasting => handle_fasting_menu(&mut conn),
-            MenuOption::Analytics => handle_analytics_menu(&mut conn),
-            MenuOption::Exit => {
-                println!("Goodbye!");
-                break;
-            }
-            MenuOption::Invalid => println!("Invalid action. Please try again."),
-        }
-    }
-}
-
-enum MenuOption {
-    Register,
-    Login,
-    Update,
-    Fasting,
-    Analytics,
-    Exit,
-    Invalid,
-}
-
-fn select_menu_option() -> MenuOption {
-    let choice = prompt_input::<String>(
-        "\nChoose an action: register, login, update, fasting, analytics, or exit: ",
-    )
-    .unwrap_or_default()
-    .to_lowercase();
-
-    match choice.as_str() {
-        "register" => MenuOption::Register,
-        "login" => MenuOption::Login,
-        "update" => MenuOption::Update,
-        "fasting" => MenuOption::Fasting,
-        "analytics" => MenuOption::Analytics,
-        "exit" => MenuOption::Exit,
-        _ => MenuOption::Invalid,
-    }
+    // Display the main menu
+    display_main_menu(&mut conn);
 }
