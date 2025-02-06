f6280c3da5da9b17709a37f56f0377493f9df585
i-ape <i-ape@proton.me>
2025-02-06 02:37:11 +0100

    error missing type, InvalidCredentials added, now have 13 errors yay

--- src/errors.rs
+++ src/errors.rs
@@ -1,22 +1,31 @@
-use bcrypt::BcryptError;
-use diesel::result::Error as DieselError;
 use thiserror::Error;
+use diesel::result::Error as DieselError;
+use bcrypt::BcryptError;
 
 /// Centralized error type for the Fasting App.
 #[derive(Debug, Error)]
 pub enum FastingAppError {
     #[error("Database error: {0}")]
     DatabaseError(#[from] DieselError),
+
     #[error("Password hash error: {0}")]
     PasswordHashError(#[from] BcryptError),
+
     #[error("An existing fasting session is already active.")]
     ExistingSessionError,
+
     #[error("Invalid request: {0}")]
     InvalidRequest(String),
+
     #[error("Connection error: {0}")]
     ConnectionError(String),
+
     #[error("Custom error: {0}")]
     Custom(String),
-    #[error("No ongoing fasting session found.")]
-    SessionError(String),
+
+    #[error("Invalid username or password.")]
+    InvalidCredentials, 
+
+    #[error("Session error: {0}")]
+    SessionError(String), 
 }
