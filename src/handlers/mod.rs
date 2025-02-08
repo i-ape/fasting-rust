1b1268e4c11295afd1362ae908fd711d5bfc80d7
i-ape <i-ape@proton.me>
2025-02-08 01:52:24 +0100

    handlers/mods refactored, rebuilt

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -3,11 +3,8 @@ pub mod fasting;
 pub mod goals;
 pub mod menu;
 
-use crate::handlers::analytics::{
-    calculate_average_fasting_duration,
-    calculate_total_fasting_time,
-    get_fasting_sessions,
-    show_fasting_history,
+pub use analytics::{
+    calculate_average_fasting_duration, calculate_total_fasting_time, show_fasting_history,
 };
 
 pub use fasting::{get_current_fasting_status, start_fasting, stop_fasting};
