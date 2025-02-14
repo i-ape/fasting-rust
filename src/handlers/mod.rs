360e02bfcd7e0efa7f6d6c803ee700efadd8bb6d
i-ape <i-ape@proton.me>
2025-02-14 16:05:38 +0100

    handler/mod update, claims its not being sourced, sad, must find reason

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -4,7 +4,18 @@ pub mod goals;
 pub mod menu;
 
 pub use analytics::{
-    calculate_average_fasting_duration, calculate_total_fasting_time, show_fasting_history,
+    calculate_average_fasting_duration, 
+    calculate_total_fasting_time, 
+    show_fasting_history,
 };
 
-pub use fasting::{get_current_fasting_status, start_fasting, stop_fasting};
+pub use fasting::{
+    get_current_fasting_status, 
+    start_fasting, 
+    stop_fasting
+};
+
+pub use goals::{
+    add_goal,
+    view_goals,
+};
