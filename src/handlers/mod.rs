66e8e68fbbfe0ee4ec3c28305429b968b48c359a
i-ape <i-ape@proton.me>
2025-02-16 18:55:30 +0100

    decision made on mod imports, i will do blanket imports so any fn current or future will function, next i will fix the error logic: variants `ExistingSessionError`, `Custom`, and `InvalidCredentials` are never constructed

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -3,22 +3,7 @@ pub mod fasting;
 pub mod goals;
 pub mod menu;
 
-// Ensure all necessary functions are accessible from handlers
-pub use analytics::{
-    calculate_average_fasting_duration, 
-    calculate_total_fasting_time, 
-    show_fasting_history,
-};
-
-pub use fasting::{
-    get_current_fasting_status, 
-    start_fasting, 
-    stop_fasting
-};
-
-pub use goals::{
-    add_goal,
-    view_goals,
-};
-
-pub use menu::display_main_menu;
+pub use analytics::*;
+pub use fasting::*;
+pub use goals::*;
+pub use menu::*;
--- src/users/mod.rs
+++ src/users/mod.rs
@@ -3,8 +3,8 @@ pub mod find;
 pub mod login;
 pub mod update;
 
-pub use create::{register_user, create_user};
-pub use login::{login_user, login_user_or_device, associate_device_id, find_user_by_device_id};
-pub use update::update_user_profile;
-pub use find::find_user_by_username;
+pub use create::*;
+pub use login::*;
+pub use update::*;
+pub use find::*;
 
