d5b085d528c87d5a0ee7caf18bc3b9a6450f3f82
i-ape <i-ape@proton.me>
2025-02-20 15:14:24 +0100

    lib update

--- src/lib.rs
+++ src/lib.rs
@@ -5,5 +5,12 @@ pub mod models;
 pub mod schema;
 pub mod users;
 
-pub use handlers::{start_fasting, stop_fasting}; // From `handlers/mod.rs`
+pub use handlers::{
+    fasting::{start_fasting, stop_fasting, get_current_fasting_status}, 
+    analytics::{calculate_average_fasting_duration, calculate_total_fasting_time, show_fasting_history},
+    goals::{add_goal, view_goals},
+};
+
 pub use users::{create_user, login_user, register_user}; // From `users/mod.rs`
+
+pub use errors::FastingAppError;
