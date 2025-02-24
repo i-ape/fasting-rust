507e1c09f1948b0295f711cc1a8fcfdc9a41d326
i-ape <i-ape@proton.me>
2025-02-24 18:05:09 +0100

    changed mind on mod import method, now user/mod has specific fn exports

--- src/users/mod.rs
+++ src/users/mod.rs
@@ -3,8 +3,8 @@ pub mod find;
 pub mod login;
 pub mod update;
 
-pub use create::*;
-pub use login::*;
-pub use update::*;
-pub use find::*;
+pub use create::{create_user, register_user};
+pub use login::{login_user, login_user_or_device, find_user_by_device_id, associate_device_id};
+pub use update::update_user_profile;
+pub use find::find_user_by_username;
 
