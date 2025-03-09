f850f52b7e9c6573b190931750c5034ba9570d1f
i-ape <i-ape@proton.me>
2025-03-09 18:20:18 +0100

    mod handlers added to fasting, update_fasting_goal and remove_fasting_goal

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -3,17 +3,13 @@ pub mod fasting;
 pub mod goals;
 pub mod menu;
 
-pub use fasting::{
-    get_current_fasting_status, 
-    start_fasting, 
-    stop_fasting, 
-    get_user_fasting_sessions,
-};
 pub use analytics::{
-    calculate_average_fasting_duration, 
-    calculate_total_fasting_time, 
-    show_fasting_history
+    calculate_average_fasting_duration, calculate_total_fasting_time, show_fasting_history,
+};
+pub use fasting::{
+    get_current_fasting_status, get_user_fasting_sessions, remove_fasting_goal, start_fasting,
+    stop_fasting, update_fasting_goal,
 };
 pub use goals::{add_goal, view_goals};
 
-pub use menu::display_main_menu; 
+pub use menu::display_main_menu;
