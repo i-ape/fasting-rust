e68b181c070c050279001873b158fdf3d15db4b7
i-ape <i-ape@proton.me>
2025-03-13 23:12:05 +0100

    main rm unused imports

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,11 @@ extern crate diesel;
 extern crate dotenv;
 
 use dotenv::dotenv;
-use std::process;
 //use log;
 
 use crate::db::establish_connection;
 use handlers::menu::display_main_menu;
-use handlers::fasting::{start_fasting, stop_fasting, get_current_fasting_status};
-use handlers::analytics::{calculate_average_fasting_duration, calculate_total_fasting_time, show_fasting_history};
-use handlers::goals::{add_goal, view_goals};
-pub use users::{create_user, login_user, register_user};
+pub use users::{create_user, login_user, login, register_user};
 
 mod db;
 mod errors;
