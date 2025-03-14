02243fa47b07dd38a804a180d16dda1edead2173
i-ape <i-ape@proton.me>
2025-03-14 20:10:41 +0100

    lib altered to declare submodules in handlers and users

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,32 @@
 pub mod db;
 pub mod errors;
-pub mod handlers;
 pub mod models;
 pub mod schema;
-pub mod users;
 
-pub use handlers::menu::display_main_menu;
-pub use handlers::fasting::{start_fasting, stop_fasting, get_current_fasting_status};
-pub use handlers::analytics::{calculate_average_fasting_duration, calculate_total_fasting_time, show_fasting_history};
+// ✅ submodules need decaration when mod is not present
+pub mod handlers {
+    pub mod analytics;
+    pub mod fasting;
+    pub mod goals;
+    pub mod menu;
+}
+
+pub mod users {
+    pub mod find;
+    pub mod login;
+    pub mod update;
+}
+
+// ✅ Publicly re-export functions so they are accessible from `lib.rs`
+pub use handlers::analytics::{
+    calculate_average_fasting_duration, calculate_total_fasting_time, show_fasting_history,
+};
+pub use handlers::fasting::{get_current_fasting_status, start_fasting, stop_fasting};
 pub use handlers::goals::{add_goal, view_goals};
+pub use handlers::menu::display_main_menu;
 
-pub use users::login::{login_user, associate_device_id};
 pub use users::find::find_user_by_id;
+pub use users::login::{associate_device_id, login_user};
 pub use users::update::update_user_profile;
- // From `users/mod.rs`
 
 pub use crate::errors::FastingAppError::*;
